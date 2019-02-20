530a1ae6a3120f1817eb453ac54addf1b1514ef5
Nguyen Khac Chinh <chinhnk>
2019-02-20 14:54:14 +0700

    optimize code

--- dist/jquery.input.js
+++ dist/jquery.input.js
@@ -95,45 +95,92 @@
     throw new TypeError("Invalid attempt to spread non-iterable instance");
   }
 
+  // 	return x;
+  // }
+
   function isArray(value) {
     return $.isArray(value);
   }
   function isObject(value) {
     return !isArray(value) && value instanceof Object;
-  }
-  function isFunction(value) {
-    return value instanceof Function;
-  }
-  function partial(f) {
-    for (var _len2 = arguments.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-      args[_key2 - 1] = arguments[_key2];
-    }
+  } // export function isFunction(value) {
+  // 	return value instanceof Function;
+  // }
+  // export function toInt(value) {
+  // 	return parseInt(value, 10);
+  // }
+  // export function bind(f, object) {
+  // 	return function (...args) {
+  // 		return f.apply(object, args);
+  // 	};
+  // }
+  // export function partial(f, ...args) {
+  // 	if (isFunction(f)) {
+  // 		return (...args1) => f(...args.concat(args1));
+  // 	}
+  // 	return undefined;
+  // }
+  // export function argumentsToArray(args) {
+  // 	const array = []; let
+  // 		i;
+  // 	for (i = 0; i < args.length; i += 1) {
+  // 		array.push(args[i]);
+  // 	}
+  // 	return array;
+  // }
+  // export function isEmpty(object) {
+  // 	return Object.keys(object).length === 0;
+  // }
+  // export function isNumeric(candidate) {
+  // 	return !Number.isNaN(candidate);
+  // }
+  // export function isInteger(candidate) {
+  // 	return isNumeric(candidate) && Number(candidate) % 1 === 0;
+  // }
 
-    if (isFunction(f)) {
-      return function () {
-        for (var _len3 = arguments.length, args1 = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-          args1[_key3] = arguments[_key3];
-        }
-
-        return f.apply(void 0, _toConsumableArray(args.concat(args1)));
-      };
-    }
-
-    return undefined;
-  }
   function indexOf(object, value) {
     return $.inArray(value, object);
   }
   function inArray(array, value) {
     return indexOf(array, value) !== -1;
-  } // deep copy of json objects
+  } // // deep copy of json objects
+  // export function copy(object) {
+  // 	return $.extend(true, {}, object);
+  // }
+  // export function shallowCopy(objects) {
+  // 	return map(objects, identity);
+  // }
+
   function foreach(collection, callback) {
     if (collection) {
       Object.keys(collection).forEach(function (i) {
         return callback(collection[i], i, collection);
       });
     }
-  }
+  } // export function range(a, b) {
+  // 	let i; let start; let end; const
+  // 		array = [];
+  // 	if (b === undefined) {
+  // 		start = 0;
+  // 		end = a - 1;
+  // 	} else {
+  // 		start = a;
+  // 		end = b;
+  // 	}
+  // 	for (i = start; i <= end; i += 1) {
+  // 		array.push(i);
+  // 	}
+  // 	return array;
+  // }
+  // export function reverse(array) {
+  // 	const reversed = []; let
+  // 		i;
+  // 	for (i = array.length - 1; i >= 0; i -= 1) {
+  // 		reversed.push(array[i]);
+  // 	}
+  // 	return reversed;
+  // }
+
   function last(array) {
     return array[array.length - 1];
   }
@@ -152,19 +199,214 @@
       mapped[key] = callback(value, key, coll);
     });
     return mapped;
-  }
+  } // export function appendKey(appendingString, collection) {
+  // 	return map(collection, identity, key => appendingString + key);
+  // }
+
   function map(collection, callback, keyCallback) {
     return isArray(collection) ? mapToArray(collection, callback) : mapToObject(collection, callback, keyCallback);
-  }
+  } // export function pluck(collection, key) {
+  // 	return map(collection, value => value[key]);
+  // }
+
   function call(collection, functionName, args) {
     return map(collection, function (object) {
       return object[functionName].apply(object, _toConsumableArray(args || []));
     });
-  }
-  function $getAnyForminatorModule(preSelector, name, moduleName) {
-    return $(preSelector + (moduleName ? "-".concat(moduleName) : '') + (name ? "-".concat(name) : ''));
-  }
-  var $getForminatorByClass = partial($getAnyForminatorModule, '.frm');
+  } // export function keys(collection) {
+  // 	return mapToArray(collection, (val, key) => key);
+  // }
+  // export function values(collection) {
+  // 	return mapToArray(collection, val => val);
+  // }
+  // export function reduce(collection, callback, initialAccumulation) {
+  // 	let accumulation = initialAccumulation;
+  // 	foreach(collection, (val, key) => {
+  // 		accumulation = callback(accumulation, val, key, collection);
+  // 	});
+  // 	return accumulation;
+  // }
+  // export function filter(collection, callback) {
+  // 	let filtered;
+  // 	if (isArray(collection)) {
+  // 		filtered = [];
+  // 		foreach(collection, (val, key, coll) => {
+  // 			if (callback(val, key, coll)) {
+  // 				filtered.push(val);
+  // 			}
+  // 		});
+  // 	} else {
+  // 		filtered = {};
+  // 		foreach(collection, (val, key, coll) => {
+  // 			if (callback(val, key, coll)) {
+  // 				filtered[key] = val;
+  // 			}
+  // 		});
+  // 	}
+  // 	return filtered;
+  // }
+  // export function union(...args) {
+  // 	const united = {}; let
+  // 		i;
+  // 	for (i = 0; i < args.length; i += 1) {
+  // 		foreach(args[i], (value, key) => {
+  // 			united[key] = value;
+  // 		});
+  // 	}
+  // 	return united;
+  // }
+  // export function subSet(object, subsetKeys) {
+  // 	return filter(object, (_, key) => indexOf(subsetKeys, key) !== -1);
+  // }
+  // export function excludedSet(object, excludedKeys) {
+  // 	return filter(object, (_, key) => indexOf(excludedKeys, key) === -1);
+  // }
+  // export function remove(collection, item) {
+  // 	return filter(collection, element => element !== item);
+  // }
+  // // call the variable if it is a function.
+  // export function callIfFunction(fn, ...args) {
+  // 	if (isFunction(fn)) {
+  // 		return fn(...args);
+  // 	}
+  // 	return undefined;
+  // }
+  // // execute callback immediately and at most one time on the minimumInterval,
+  // // ignore block attempts
+  // export function throttle(minimumInterval, callback) {
+  // 	let timeout = null;
+  // 	return function (...args) {
+  // 		const that = this;
+  // 		if (timeout === null) {
+  // 			timeout = setTimeout(() => {
+  // 				timeout = null;
+  // 			}, minimumInterval);
+  // 			callback.apply(that, args);
+  // 		}
+  // 	};
+  // }
+  // // execute callback at most one time on the minimumInterval
+  // export function debounce(minimumInterval, callback, isImmediate) {
+  // 	let timeout = null;
+  // 	let isAttemptBlockedOnInterval = false;
+  // 	return function (...args) {
+  // 		const that = this;
+  // 		if (timeout === null) {
+  // 			timeout = setTimeout(() => {
+  // 				if (!isImmediate || isAttemptBlockedOnInterval) {
+  // 					callback.apply(that, args);
+  // 				}
+  // 				isAttemptBlockedOnInterval = false;
+  // 				timeout = null;
+  // 			}, minimumInterval);
+  // 			if (isImmediate) {
+  // 				callback.apply(that, args);
+  // 			}
+  // 		} else {
+  // 			isAttemptBlockedOnInterval = true;
+  // 		}
+  // 	};
+  // }
+  // export const generateUniqueID = (function () {
+  // 	let count = 0;
+  // 	return function () {
+  // 		count += 1;
+  // 		return count;
+  // 	};
+  // }());
+  // // export function mixinPubSub(object) {
+  // // 	object = object || {};
+  // // 	const topics = {};
+  // // 	object.publish = function (topic, data) {
+  // // 		foreach(topics[topic], (callback) => {
+  // // 			callback(data);
+  // // 		});
+  // // 	};
+  // // 	object.subscribe = function (topic, callback) {
+  // // 		topics[topic] = topics[topic] || [];
+  // // 		topics[topic].push(callback);
+  // // 	};
+  // // 	object.unsubscribe = function (callback) {
+  // // 		foreach(topics, (subscribers) => {
+  // // 			const index = indexOf(subscribers, callback);
+  // // 			if (index !== -1) {
+  // // 				subscribers.splice(index, 1);
+  // // 			}
+  // // 		});
+  // // 	};
+  // // 	return object;
+  // // }
+  // // queryjs
+  // // https://github.com/DubFriend/queryjs
+  // // MIT License 2014 Brian Detering
+  // export const queryjs = (function () {
+  // 	const qjs = {};
+  // 	const parse = function (url) {
+  // 		let domain = ''; let
+  // 			hash = '';
+  // 		const getParameterStrings = function () {
+  // 			const isHash = url.indexOf('#') !== -1;
+  // 			const isQuery = url.indexOf('?') !== -1;
+  // 			let queryString = '';
+  // 			if (isQuery) {
+  // 				queryString = url.split('?')[1] || '';
+  // 				if (isHash) {
+  // 					queryString = queryString.split('#')[0] || '';
+  // 				}
+  // 			}
+  // 			if (isQuery) {
+  // 				domain = url.split('?')[0] || '';
+  // 			} else if (isHash) {
+  // 				domain = url.split('#')[0] || '';
+  // 			} else {
+  // 				domain = url;
+  // 			}
+  // 			if (isHash) {
+  // 				hash = url.split('#')[1] || '';
+  // 			}
+  // 			return queryString ? queryString.split('&') : [];
+  // 		};
+  // 		const parameterStrings = getParameterStrings(url);
+  // 		const params = {};
+  // 		let key; let value; let i;
+  // 		for (i = 0; i < parameterStrings.length; i += 1) {
+  // 			[key, value] = parameterStrings[i].split('=');
+  // 			params[key] = value;
+  // 		}
+  // 		return {
+  // 			url: domain || '',
+  // 			hash: hash || '',
+  // 			parameters: params,
+  // 		};
+  // 	};
+  // 	const stringify = function (parsed) {
+  // 		const parameterStrings = [];
+  // 		foreach(parsed.parameters, (_, key) => {
+  // 			parameterStrings.push(`${key}=${parsed.parameters[key]}`);
+  // 		});
+  // 		return parsed.url
+  // 			+ (parameterStrings.length > 0
+  // 				? `?${parameterStrings.join('&')}` : '')
+  // 			+ (parsed.hash ? `#${parsed.hash}` : '');
+  // 	};
+  // 	qjs.get = function (url) {
+  // 		return parse(url).parameters;
+  // 	};
+  // 	qjs.set = function (url, params) {
+  // 		const parsed = parse(url);
+  // 		parsed.parameters = union(parsed.parameters, params);
+  // 		return stringify(parsed);
+  // 	};
+  // 	return qjs;
+  // }());
+  // export function $getAnyForminatorModule(preSelector, name, moduleName) {
+  // 	return $(
+  // 		preSelector
+  // 		+ (moduleName ? `-${moduleName}` : '')
+  // 		+ (name ? `-${name}` : ''),
+  // 	);
+  // }
+  // export const $getForminatorByClass = partial($getAnyForminatorModule, '.frm');
 
   var PubSub =
   /*#__PURE__*/
@@ -514,11 +756,11 @@
       value: function get() {
         var _this2 = this;
 
-        var values$$1 = [];
+        var values = [];
         this.$().filter(':checked').each(function (_, ele) {
-          values$$1.push($(_this2).val(ele));
+          values.push($(_this2).val(ele));
         });
-        return values$$1;
+        return values;
       }
     }, {
       key: "set",
--- src/library.js
+++ src/library.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
 import $ from 'jquery';
 
-export function identity(x) {
-	return x;
-}
+// export function identity(x) {
+// 	return x;
+// }
 
 export function isArray(value) {
 	return $.isArray(value);
@@ -13,48 +13,48 @@ export function isObject(value) {
 	return !isArray(value) && (value instanceof Object);
 }
 
-export function isFunction(value) {
-	return value instanceof Function;
-}
+// export function isFunction(value) {
+// 	return value instanceof Function;
+// }
 
-export function toInt(value) {
-	return parseInt(value, 10);
-}
+// export function toInt(value) {
+// 	return parseInt(value, 10);
+// }
 
-export function bind(f, object) {
-	return function (...args) {
-		return f.apply(object, args);
-	};
-}
+// export function bind(f, object) {
+// 	return function (...args) {
+// 		return f.apply(object, args);
+// 	};
+// }
 
-export function partial(f, ...args) {
-	if (isFunction(f)) {
-		return (...args1) => f(...args.concat(args1));
-	}
+// export function partial(f, ...args) {
+// 	if (isFunction(f)) {
+// 		return (...args1) => f(...args.concat(args1));
+// 	}
 
-	return undefined;
-}
+// 	return undefined;
+// }
 
-export function argumentsToArray(args) {
-	const array = []; let
-		i;
-	for (i = 0; i < args.length; i += 1) {
-		array.push(args[i]);
-	}
-	return array;
-}
+// export function argumentsToArray(args) {
+// 	const array = []; let
+// 		i;
+// 	for (i = 0; i < args.length; i += 1) {
+// 		array.push(args[i]);
+// 	}
+// 	return array;
+// }
 
-export function isEmpty(object) {
-	return Object.keys(object).length === 0;
-}
+// export function isEmpty(object) {
+// 	return Object.keys(object).length === 0;
+// }
 
-export function isNumeric(candidate) {
-	return !Number.isNaN(candidate);
-}
+// export function isNumeric(candidate) {
+// 	return !Number.isNaN(candidate);
+// }
 
-export function isInteger(candidate) {
-	return isNumeric(candidate) && Number(candidate) % 1 === 0;
-}
+// export function isInteger(candidate) {
+// 	return isNumeric(candidate) && Number(candidate) % 1 === 0;
+// }
 
 export function indexOf(object, value) {
 	return $.inArray(value, object);
@@ -64,14 +64,14 @@ export function inArray(array, value) {
 	return indexOf(array, value) !== -1;
 }
 
-// deep copy of json objects
-export function copy(object) {
-	return $.extend(true, {}, object);
-}
+// // deep copy of json objects
+// export function copy(object) {
+// 	return $.extend(true, {}, object);
+// }
 
-export function shallowCopy(objects) {
-	return map(objects, identity);
-}
+// export function shallowCopy(objects) {
+// 	return map(objects, identity);
+// }
 
 export function foreach(collection, callback) {
 	if (collection) {
@@ -80,30 +80,30 @@ export function foreach(collection, callback) {
 	}
 }
 
-export function range(a, b) {
-	let i; let start; let end; const
-		array = [];
-	if (b === undefined) {
-		start = 0;
-		end = a - 1;
-	} else {
-		start = a;
-		end = b;
-	}
-	for (i = start; i <= end; i += 1) {
-		array.push(i);
-	}
-	return array;
-}
+// export function range(a, b) {
+// 	let i; let start; let end; const
+// 		array = [];
+// 	if (b === undefined) {
+// 		start = 0;
+// 		end = a - 1;
+// 	} else {
+// 		start = a;
+// 		end = b;
+// 	}
+// 	for (i = start; i <= end; i += 1) {
+// 		array.push(i);
+// 	}
+// 	return array;
+// }
 
-export function reverse(array) {
-	const reversed = []; let
-		i;
-	for (i = array.length - 1; i >= 0; i -= 1) {
-		reversed.push(array[i]);
-	}
-	return reversed;
-}
+// export function reverse(array) {
+// 	const reversed = []; let
+// 		i;
+// 	for (i = array.length - 1; i >= 0; i -= 1) {
+// 		reversed.push(array[i]);
+// 	}
+// 	return reversed;
+// }
 
 export function last(array) {
 	return array[array.length - 1];
@@ -127,9 +127,9 @@ export function mapToObject(collection, callback, keyCallback) {
 	return mapped;
 }
 
-export function appendKey(appendingString, collection) {
-	return map(collection, identity, key => appendingString + key);
-}
+// export function appendKey(appendingString, collection) {
+// 	return map(collection, identity, key => appendingString + key);
+// }
 
 export function map(collection, callback, keyCallback) {
 	return isArray(collection)
@@ -137,9 +137,9 @@ export function map(collection, callback, keyCallback) {
 		: mapToObject(collection, callback, keyCallback);
 }
 
-export function pluck(collection, key) {
-	return map(collection, value => value[key]);
-}
+// export function pluck(collection, key) {
+// 	return map(collection, value => value[key]);
+// }
 
 export function call(collection, functionName, args) {
 	return map(
@@ -148,241 +148,241 @@ export function call(collection, functionName, args) {
 	);
 }
 
-export function keys(collection) {
-	return mapToArray(collection, (val, key) => key);
-}
+// export function keys(collection) {
+// 	return mapToArray(collection, (val, key) => key);
+// }
 
-export function values(collection) {
-	return mapToArray(collection, val => val);
-}
+// export function values(collection) {
+// 	return mapToArray(collection, val => val);
+// }
 
-export function reduce(collection, callback, initialAccumulation) {
-	let accumulation = initialAccumulation;
-	foreach(collection, (val, key) => {
-		accumulation = callback(accumulation, val, key, collection);
-	});
-	return accumulation;
-}
+// export function reduce(collection, callback, initialAccumulation) {
+// 	let accumulation = initialAccumulation;
+// 	foreach(collection, (val, key) => {
+// 		accumulation = callback(accumulation, val, key, collection);
+// 	});
+// 	return accumulation;
+// }
 
-export function filter(collection, callback) {
-	let filtered;
+// export function filter(collection, callback) {
+// 	let filtered;
 
-	if (isArray(collection)) {
-		filtered = [];
-		foreach(collection, (val, key, coll) => {
-			if (callback(val, key, coll)) {
-				filtered.push(val);
-			}
-		});
-	} else {
-		filtered = {};
-		foreach(collection, (val, key, coll) => {
-			if (callback(val, key, coll)) {
-				filtered[key] = val;
-			}
-		});
-	}
-
-	return filtered;
-}
-
-export function union(...args) {
-	const united = {}; let
-		i;
-	for (i = 0; i < args.length; i += 1) {
-		foreach(args[i], (value, key) => {
-			united[key] = value;
-		});
-	}
-	return united;
-}
-
-export function subSet(object, subsetKeys) {
-	return filter(object, (_, key) => indexOf(subsetKeys, key) !== -1);
-}
-
-export function excludedSet(object, excludedKeys) {
-	return filter(object, (_, key) => indexOf(excludedKeys, key) === -1);
-}
-
-export function remove(collection, item) {
-	return filter(collection, element => element !== item);
-}
-
-// call the variable if it is a function.
-export function callIfFunction(fn, ...args) {
-	if (isFunction(fn)) {
-		return fn(...args);
-	}
-
-	return undefined;
-}
-
-
-// execute callback immediately and at most one time on the minimumInterval,
-// ignore block attempts
-export function throttle(minimumInterval, callback) {
-	let timeout = null;
-	return function (...args) {
-		const that = this;
-		if (timeout === null) {
-			timeout = setTimeout(() => {
-				timeout = null;
-			}, minimumInterval);
-			callback.apply(that, args);
-		}
-	};
-}
-
-// execute callback at most one time on the minimumInterval
-export function debounce(minimumInterval, callback, isImmediate) {
-	let timeout = null;
-	let isAttemptBlockedOnInterval = false;
-	return function (...args) {
-		const that = this;
-		if (timeout === null) {
-			timeout = setTimeout(() => {
-				if (!isImmediate || isAttemptBlockedOnInterval) {
-					callback.apply(that, args);
-				}
-				isAttemptBlockedOnInterval = false;
-				timeout = null;
-			}, minimumInterval);
-			if (isImmediate) {
-				callback.apply(that, args);
-			}
-		} else {
-			isAttemptBlockedOnInterval = true;
-		}
-	};
-}
-
-export const generateUniqueID = (function () {
-	let count = 0;
-	return function () {
-		count += 1;
-		return count;
-	};
-}());
-
-// export function mixinPubSub(object) {
-// 	object = object || {};
-// 	const topics = {};
-
-// 	object.publish = function (topic, data) {
-// 		foreach(topics[topic], (callback) => {
-// 			callback(data);
-// 		});
-// 	};
-
-// 	object.subscribe = function (topic, callback) {
-// 		topics[topic] = topics[topic] || [];
-// 		topics[topic].push(callback);
-// 	};
-
-// 	object.unsubscribe = function (callback) {
-// 		foreach(topics, (subscribers) => {
-// 			const index = indexOf(subscribers, callback);
-// 			if (index !== -1) {
-// 				subscribers.splice(index, 1);
+// 	if (isArray(collection)) {
+// 		filtered = [];
+// 		foreach(collection, (val, key, coll) => {
+// 			if (callback(val, key, coll)) {
+// 				filtered.push(val);
 // 			}
 // 		});
-// 	};
+// 	} else {
+// 		filtered = {};
+// 		foreach(collection, (val, key, coll) => {
+// 			if (callback(val, key, coll)) {
+// 				filtered[key] = val;
+// 			}
+// 		});
+// 	}
 
-// 	return object;
+// 	return filtered;
+// }
+
+// export function union(...args) {
+// 	const united = {}; let
+// 		i;
+// 	for (i = 0; i < args.length; i += 1) {
+// 		foreach(args[i], (value, key) => {
+// 			united[key] = value;
+// 		});
+// 	}
+// 	return united;
+// }
+
+// export function subSet(object, subsetKeys) {
+// 	return filter(object, (_, key) => indexOf(subsetKeys, key) !== -1);
+// }
+
+// export function excludedSet(object, excludedKeys) {
+// 	return filter(object, (_, key) => indexOf(excludedKeys, key) === -1);
+// }
+
+// export function remove(collection, item) {
+// 	return filter(collection, element => element !== item);
+// }
+
+// // call the variable if it is a function.
+// export function callIfFunction(fn, ...args) {
+// 	if (isFunction(fn)) {
+// 		return fn(...args);
+// 	}
+
+// 	return undefined;
 // }
 
 
-// queryjs
-// https://github.com/DubFriend/queryjs
-// MIT License 2014 Brian Detering
-export const queryjs = (function () {
-	const qjs = {};
+// // execute callback immediately and at most one time on the minimumInterval,
+// // ignore block attempts
+// export function throttle(minimumInterval, callback) {
+// 	let timeout = null;
+// 	return function (...args) {
+// 		const that = this;
+// 		if (timeout === null) {
+// 			timeout = setTimeout(() => {
+// 				timeout = null;
+// 			}, minimumInterval);
+// 			callback.apply(that, args);
+// 		}
+// 	};
+// }
 
-	const parse = function (url) {
-		let domain = ''; let
-			hash = '';
-		const getParameterStrings = function () {
-			const isHash = url.indexOf('#') !== -1;
+// // execute callback at most one time on the minimumInterval
+// export function debounce(minimumInterval, callback, isImmediate) {
+// 	let timeout = null;
+// 	let isAttemptBlockedOnInterval = false;
+// 	return function (...args) {
+// 		const that = this;
+// 		if (timeout === null) {
+// 			timeout = setTimeout(() => {
+// 				if (!isImmediate || isAttemptBlockedOnInterval) {
+// 					callback.apply(that, args);
+// 				}
+// 				isAttemptBlockedOnInterval = false;
+// 				timeout = null;
+// 			}, minimumInterval);
+// 			if (isImmediate) {
+// 				callback.apply(that, args);
+// 			}
+// 		} else {
+// 			isAttemptBlockedOnInterval = true;
+// 		}
+// 	};
+// }
+
+// export const generateUniqueID = (function () {
+// 	let count = 0;
+// 	return function () {
+// 		count += 1;
+// 		return count;
+// 	};
+// }());
+
+// // export function mixinPubSub(object) {
+// // 	object = object || {};
+// // 	const topics = {};
+
+// // 	object.publish = function (topic, data) {
+// // 		foreach(topics[topic], (callback) => {
+// // 			callback(data);
+// // 		});
+// // 	};
+
+// // 	object.subscribe = function (topic, callback) {
+// // 		topics[topic] = topics[topic] || [];
+// // 		topics[topic].push(callback);
+// // 	};
+
+// // 	object.unsubscribe = function (callback) {
+// // 		foreach(topics, (subscribers) => {
+// // 			const index = indexOf(subscribers, callback);
+// // 			if (index !== -1) {
+// // 				subscribers.splice(index, 1);
+// // 			}
+// // 		});
+// // 	};
+
+// // 	return object;
+// // }
 
 
-			const isQuery = url.indexOf('?') !== -1;
+// // queryjs
+// // https://github.com/DubFriend/queryjs
+// // MIT License 2014 Brian Detering
+// export const queryjs = (function () {
+// 	const qjs = {};
+
+// 	const parse = function (url) {
+// 		let domain = ''; let
+// 			hash = '';
+// 		const getParameterStrings = function () {
+// 			const isHash = url.indexOf('#') !== -1;
 
 
-			let queryString = '';
+// 			const isQuery = url.indexOf('?') !== -1;
 
-			if (isQuery) {
-				queryString = url.split('?')[1] || '';
-				if (isHash) {
-					queryString = queryString.split('#')[0] || '';
-				}
-			}
 
-			if (isQuery) {
-				domain = url.split('?')[0] || '';
-			} else if (isHash) {
-				domain = url.split('#')[0] || '';
-			} else {
-				domain = url;
-			}
+// 			let queryString = '';
 
-			if (isHash) {
-				hash = url.split('#')[1] || '';
-			}
+// 			if (isQuery) {
+// 				queryString = url.split('?')[1] || '';
+// 				if (isHash) {
+// 					queryString = queryString.split('#')[0] || '';
+// 				}
+// 			}
 
-			return queryString ? queryString.split('&') : [];
-		};
+// 			if (isQuery) {
+// 				domain = url.split('?')[0] || '';
+// 			} else if (isHash) {
+// 				domain = url.split('#')[0] || '';
+// 			} else {
+// 				domain = url;
+// 			}
 
-		const parameterStrings = getParameterStrings(url);
+// 			if (isHash) {
+// 				hash = url.split('#')[1] || '';
+// 			}
 
-		const params = {};
+// 			return queryString ? queryString.split('&') : [];
+// 		};
 
-		let key; let value; let i;
+// 		const parameterStrings = getParameterStrings(url);
 
-		for (i = 0; i < parameterStrings.length; i += 1) {
-			[key, value] = parameterStrings[i].split('=');
-			params[key] = value;
-		}
+// 		const params = {};
 
-		return {
-			url: domain || '',
-			hash: hash || '',
-			parameters: params,
-		};
-	};
+// 		let key; let value; let i;
 
-	const stringify = function (parsed) {
-		const parameterStrings = [];
+// 		for (i = 0; i < parameterStrings.length; i += 1) {
+// 			[key, value] = parameterStrings[i].split('=');
+// 			params[key] = value;
+// 		}
 
-		foreach(parsed.parameters, (_, key) => {
-			parameterStrings.push(`${key}=${parsed.parameters[key]}`);
-		});
+// 		return {
+// 			url: domain || '',
+// 			hash: hash || '',
+// 			parameters: params,
+// 		};
+// 	};
 
-		return parsed.url
-			+ (parameterStrings.length > 0
-				? `?${parameterStrings.join('&')}` : '')
-			+ (parsed.hash ? `#${parsed.hash}` : '');
-	};
+// 	const stringify = function (parsed) {
+// 		const parameterStrings = [];
 
-	qjs.get = function (url) {
-		return parse(url).parameters;
-	};
+// 		foreach(parsed.parameters, (_, key) => {
+// 			parameterStrings.push(`${key}=${parsed.parameters[key]}`);
+// 		});
 
-	qjs.set = function (url, params) {
-		const parsed = parse(url);
-		parsed.parameters = union(parsed.parameters, params);
-		return stringify(parsed);
-	};
+// 		return parsed.url
+// 			+ (parameterStrings.length > 0
+// 				? `?${parameterStrings.join('&')}` : '')
+// 			+ (parsed.hash ? `#${parsed.hash}` : '');
+// 	};
 
-	return qjs;
-}());
+// 	qjs.get = function (url) {
+// 		return parse(url).parameters;
+// 	};
 
-export function $getAnyForminatorModule(preSelector, name, moduleName) {
-	return $(
-		preSelector
-		+ (moduleName ? `-${moduleName}` : '')
-		+ (name ? `-${name}` : ''),
-	);
-}
+// 	qjs.set = function (url, params) {
+// 		const parsed = parse(url);
+// 		parsed.parameters = union(parsed.parameters, params);
+// 		return stringify(parsed);
+// 	};
 
-export const $getForminatorByClass = partial($getAnyForminatorModule, '.frm');
+// 	return qjs;
+// }());
+
+// export function $getAnyForminatorModule(preSelector, name, moduleName) {
+// 	return $(
+// 		preSelector
+// 		+ (moduleName ? `-${moduleName}` : '')
+// 		+ (name ? `-${name}` : ''),
+// 	);
+// }
+
+// export const $getForminatorByClass = partial($getAnyForminatorModule, '.frm');
